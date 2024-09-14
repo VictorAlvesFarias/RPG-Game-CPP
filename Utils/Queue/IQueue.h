9f328f965fbf758fc052c4dcb5bf8e3535ca94b8
Vitinhoooo <barbosavictor739@gmail.com>
2024-09-14 13:33:48 -0300

    FIla

---
+++ Utils/Queue/IQueue.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class Fila {
+public:
+	Fila();
+	~Fila();
+	bool append(int genérico); //retorna se deu certo?
+	int serve();
+	bool empty();
+	bool full();
+	int size();
+	void clear();
+	int front();
+	int rear();
+
+private:
+	struct NodeFila
+	{
+		int valorGenerico;
+		NodeFila* proximoFila;
+	};
+
+	int contador;
+	NodeFila* head;
+	NodeFila* tail;
+};
